7676a7244b644fdcc9e7032be6ceda364829562b
Dharmesh Malam <dmmalam@gmail.com>
2011-04-03 21:00:50 +0100

    Added keybinding file

---
+++ plugins/natural-4000/natural-4000.plugin.zsh
@@ -0,0 +1,17 @@
+# Keybindings for MS Natural Keyboard 4000
+
+
+bindkey "^[[3~" delete-char       
+bindkey "^[[4~" end-of-line 
+bindkey "^[[1~" beginning-of-line
+bindkey '^[[5D' emacs-backward-word   
+bindkey '^[[5C' emacs-forward-word  
+
+bindkey "^[[A" history-beginning-search-backward
+
+bindkey "^[[B" history-beginning-search-forward
+
+#bindkey "" backward-delete-word
+
+#bindkey "" delete-word
+#bindkey "" backward-kill-line
